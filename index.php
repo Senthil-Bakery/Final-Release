92c23cefbb72cb7d0b1c01c673a53d9d46569717
jc434424 <sangha91@gmail.com>
2017-09-13 18:38:27 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Senthil Bakery</title>
+<link rel="stylesheet" href="css/main.css">
+
+
+
+</head>
+
+<body>
+
+	
+	<?php include 'header.php';?>
+	<div class="row slider">
+	
+		</div>
+	
+	
+	<div class="row">
+	<div class="content col-12">
+		<h2>Welcome</h2>
+		<p style="text-align: justify;">Senthil Bakery located in Chennai, Tamil Nadu, India was established in 1988, it
+was small shop when the business is started and now it is grown to a business
+where all similar small business shops from its location buy Senthil Bakery
+products in whole sale to sell in their shop. Product being sold in whole sale is the
+reason the branches of Street Corner were not opened. Senthil Bakery is now
+synonymous with delightfulness to manufacture high quality and mouthwatering
+sweets, savories, bakery products and snacks. Other company products
+sold in our shop are Uncle Johns Ice cream and soft drinks from Coca Cola and
+Pepsi. We manufacture products in a hygiene environment with we trained staff
+member. We hire staff member with good character and basic eligibility and we 
+3
+train them in a friendly environment to get skilled. We believe growth in staff
+			member will lead to business growth.</p>
+		<h3>MISSION</h3>
+<p>“To deliver delightfulness to all customers through customer service, quality and
+taste of the product”
+</p>
+	</div>
+	</div>
+		
+		
+	
+	<?php include 'footer.php';?>
+	
+</div>
+
+</body>
+</html>
